79b100e762b767ae9e6509904d0fcc83b6e8916b
Nathan Ringo <remexre@gmail.com>
2019-11-08 21:32:48 -0600

    ...

--- components/flubber-server/src/main.rs
+++ components/flubber-server/src/main.rs
@@ -1,7 +1,8 @@
-use anyhow::Result;
+use anyhow::{anyhow, Context, Result};
 use flubber_server::{start_http, Backend, Config, Database};
+use futures_util::stream::select_all;
 use std::path::PathBuf;
-use tokio::runtime::Runtime;
+use tokio::{future::ready, prelude::*, runtime::Runtime};
 
 #[derive(Debug, structopt::StructOpt)]
 struct Args {
@@ -25,8 +26,58 @@ fn main(args: Args) -> Result<()> {
     let runtime = Runtime::new()?;
     runtime.block_on(async {
         let mut configs = Config::watch(args.config_file).await?;
-        let config = configs.recv().await;
-        log::info!("{:#?}", config);
+        let config = configs
+            .recv()
+            .await
+            .ok_or_else(|| anyhow!("Config watcher died?"))?;
+
+        let database = Database::open(config.database.file)?;
+
+        let backends = config
+            .backends
+            .into_iter()
+            .map(|(name, mut config)| {
+                if let Some(path) = config.env_file {
+                    config.env.extend(
+                        // TODO: This is sync. It also only runs during initialization, but
+                        // still...
+                        parse_dotenv(path)
+                            .with_context(|| format!("Failed to load env file for {}", name))?,
+                    );
+                }
+                Ok((name, Backend::new(config.cmd, config.args, config.env)))
+            })
+            .collect::<Result<Vec<_>>>()?;
+        let senders = backends
+            .iter()
+            .map(|(name, backend)| (name.clone(), backend.sender()))
+            .collect::<Vec<_>>();
+        let updates = select_all(
+            backends
+                .into_iter()
+                .map(|(name, backend)| backend.map(move |u| (name.clone(), u))),
+        );
+
+        if let Some(web) = config.web {
+            runtime.spawn(start_http(database.clone(), (web.addr, web.port)));
+        }
+
+        runtime.spawn(configs.for_each(|config| {
+            log::warn!("Configuration updates NYI.");
+            log::info!("config = {:?}", config);
+            ready(())
+        }));
+
+        log::info!("{:#?}", senders);
+        log::info!("{:#?}", updates);
+
         Ok(())
     })
+}
+
+#[allow(deprecated)]
+fn parse_dotenv(path: PathBuf) -> Result<Vec<(String, String)>> {
+    dotenv::from_path_iter(path)?
+        .map(|r| r.map_err(Into::into))
+        .collect()
 }
